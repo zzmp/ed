fd5f8f7b104904129b9e8f555afeeb32f27f3253
zzmp <zmp@umich.edu>
2014-03-18 14:22:15 -0500

    View: initial elements

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+$(document).ready( function() {
+  $('#collaborate').on( 'mouseenter', function() {
+    $(this).text( 'collaborate and listen' );
+  });
 
+  $('#collaborate').on( 'mouseleave', function() {
+    $(this).text( 'stop!' );
+  });
+});
