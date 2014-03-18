b60d38f7778da30c1c256aa0a722283f82f19a1e
zzmp <zmp@umich.edu>
2014-03-18 18:26:00 -0500

    Cover is animated on first scroll

--- js/main.js
+++ js/main.js
@@ -6,4 +6,28 @@ $(document).ready( function() {
   $('#collaborate').on( 'mouseleave', function() {
     $(this).text( 'stop' );
   });
+
+  var uncover = function() {
+    var $cover = $('.cover');
+
+    $cover.find( 'img' ).animate(
+      { borderSpacing: -720 },
+      {
+        step: function( now, fx) {
+          $(this).css( '-webkit-transform', 'rotate(' + now + 'deg)' );
+          $(this).css( '-moz-transform', 'rotate(' + now + 'deg)' );
+          $(this).css( 'transform', 'rotate(' + now + 'deg)' );
+        },
+        duration:'slow'
+      }
+    );
+
+    $.when( $cover.find( 'img' ).promise() ).then( function() {
+      $cover.animate( {left: '100%'}, 2000, '', function() {
+        $('footer').show();
+      });
+    });
+  }
+
+  $(window).one( 'scroll', uncover );
 });
