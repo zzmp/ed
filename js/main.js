fb56c3e4a293f132303428e26a6f2ce693d55285
zzmp <zmp@umich.edu>
2014-03-19 02:10:22 -0500

    Ed functionality up, lacks insulating frame element

--- js/main.js
+++ js/main.js
@@ -1,16 +1,27 @@
 $(document).ready( function() {
+
   var $html = $('#html');
   var $css = $('#css');
   var $js = $('#js');
+  var $view = $('.ed.view');
+  var $style = $('<style></style>');
+  var $script = $('<script></script');
+  $('body').append($style).append($script);
   var review = function( isHtmlChanged, isCssChanged, isJsChanged ) {
-    var updateCss = function() {};
-    var updateHtml = function() {};
-    var updateJs = function() {};
+    var updateCss = function() {
+      $style.html($css.text());
+    };
+    var updateHtml = function() {
+      $view.html($html.text());
+    };
+    var updateJs = function() {
+      $script.html($js.text());
+    };
 
     if ( isCssChanged ) { updateCss(); }
     if ( isHtmlChanged ) { updateHtml(); }
     if ( isJsChanged ) { updateJs(); }
-  }
+  }(true, true, true);
 
   var uncover = function() {
     var $cover = $('.cover');
