af6b08f422dfa1d6b9eaf8b4e58e5fb3976eec30
zzmp <zmp@umich.edu>
2014-03-19 01:54:31 -0500

    Ed functionality framework

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 $(document).ready( function() {
-  
+  var $html = $('#html');
+  var $css = $('#css');
+  var $js = $('#js');
+  var review = function( isHtmlChanged, isCssChanged, isJsChanged ) {
+    var updateCss = function() {};
+    var updateHtml = function() {};
+    var updateJs = function() {};
+
+    if ( isCssChanged ) { updateCss(); }
+    if ( isHtmlChanged ) { updateHtml(); }
+    if ( isJsChanged ) { updateJs(); }
+  }
 
   var uncover = function() {
     var $cover = $('.cover');
